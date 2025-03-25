94d893ed32306396a4fdcb64ebecf81ebdb720c5
Judah Ankrah <s22129511@bcud2-024465.students.uce.ac.uk>
2025-03-25 12:15:02 +0000

    assessmenttrial

---
+++ build/temp.m
@@ -0,0 +1,15 @@
+%%assement work
+
+audioread()
+audiowrite()
+conv()
+filter()
+normalize()
+
+%applyng a filter
+y = filter(b, a, x);
+fs = 44100;
+fc = 100;
+[b, a] = butter(2, fc/(fs2));
+
+%task
