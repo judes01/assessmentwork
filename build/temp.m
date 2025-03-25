ddb059b0e845b1a64d3e804d5f08d830c20cfced
judes01 <judah.ankrah@mail.bcu.ac.uk>
2025-03-25 14:56:47 +0000

    Update temp.m

--- build/temp.m
+++ build/temp.m
@@ -1,10 +1,10 @@
-%%assement work
+%%assessment work
 
-audioread()
-audiowrite()
-conv()
-filter()
-normalize()
+audioread() % Reads an audio file and returns the audio data and sample rate.
+audiowrite() % Writes audio data to a file with the specified format and sample rate.
+conv() % Performs convolution of two signals or arrays. It computes the sum of the product of two sequences over a range of time.
+filter() % Applies a filter to a signal (e.g., low-pass, high-pass) by using the given filter coefficients.
+normalize() % Adjusts the amplitude of an audio signal to ensure it stays within a specified range (typically [-1, 1]).
 
  % i have to use the functions above for my code 
 %applyng a filter
